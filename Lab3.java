44fdd8a265cc1763bfcbf29425a83efa92ccee39
zhaoyangyingmu <1065348898@qq.com>
2019-10-08 10:32:55 +0800

    first commit

---
+++ Lab3.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Lab3 {
+    public static void main(String[] args) {
+        String dateString = JOptionPane.showInputDialog("输入日期");
+        String constellation = "天蝎座";
+        // 1. 将dateString转化为year，month和day。
+        // 2. 判断星座类型。
+
+        // 3. 输出星座类型
+        JOptionPane.showMessageDialog(null, constellation);
+    }
+}
